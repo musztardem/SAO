373f42d17d95aa90f908dab7bade9658540a2b67
Antek <antoni.pstras@gmail.com>
2017-03-06 19:06:31 +0100

    drawing background net

---
+++ colors.js
@@ -0,0 +1,6 @@
+const Colors = {
+  grey: '#DFDCE3',
+  yellow: '#F7B733',
+  orange: '#FC4A1A',
+  blue: '#4ABDAC'
+};
--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const canvas = document.getElementById('board');
 const ctx = canvas.getContext('2d');
+const NET_FACTOR = 50;
 
-ctx.fillStyle = 'rgb(200, 0, 0)';
-ctx.fillRect(10, 10, 50, 50);
+const drawBackground = () => {
+  /* background color */
+  ctx.fillStyle = Colors.grey;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-ctx.fillRect(30, 30, 50, 50);
+  /* net */
+  ctx.strokeStyle = Colors.blue;
+  for (let i = 0; i < canvas.width / NET_FACTOR; i++) {
+    ctx.beginPath();
+    ctx.moveTo(i * NET_FACTOR, 0);
+    ctx.lineTo(i * NET_FACTOR, canvas.height);
+    ctx.stroke();
+  }
+
+  for (let i = 0; i < canvas.height / NET_FACTOR; i++) {
+    ctx.beginPath();
+    ctx.moveTo(0, i * NET_FACTOR);
+    ctx.lineTo(canvas.width, i * NET_FACTOR);
+    ctx.stroke();
+  }
+}
+
+drawBackground();
