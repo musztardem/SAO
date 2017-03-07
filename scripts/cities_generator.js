2d3a90affca6869d12b24318bdacffe395575519
Antek <antoni.pstras@gmail.com>
2017-03-07 11:10:52 +0100

    preventing CitiesGenerator from generating duplicated cities

--- scripts/cities_generator.js
+++ scripts/cities_generator.js
@@ -8,13 +8,24 @@ class CitiesGenerator {
   }
 
   generate() {
+    const cityIsUnique = (cities, x, y) => {
+      return !cities.some(city => {
+        return city.x == x && city.y == y;
+      });
+    }
+
     let cities = [];
     let xMax = this._maxWidth / COORD_DIVISOR;
     let yMax = this._maxHeight / COORD_DIVISOR;
 
     for (let i = 0; i < this._citiesCount; i++) {
-      let xCoord = Utils.getRandomNumber(0, xMax) * COORD_DIVISOR;
-      let yCoord = Utils.getRandomNumber(0, yMax) * COORD_DIVISOR;
+      let xCoord, yCoord;
+      while(true) {
+        xCoord = Utils.getRandomNumber(0, xMax) * COORD_DIVISOR;
+        yCoord = Utils.getRandomNumber(0, yMax) * COORD_DIVISOR;
+        if (cityIsUnique(cities, xCoord, yCoord)) break;
+      }
+
       cities.push({ x: xCoord, y: yCoord });
     }
 
